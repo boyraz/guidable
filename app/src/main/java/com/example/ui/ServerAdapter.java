93fda21e754dd6cc082f58f4bd54a5930095dc9a
boyraz <filiz.boyraz@hotmail.com>
2015-11-28 23:18:49 +0900

    Update ServerAdapter.java
    implementation

--- app/src/main/java/com/example/ui/ServerAdapter.java
+++ app/src/main/java/com/example/ui/ServerAdapter.java
@@ -6,15 +6,26 @@ import java.util.ArrayList;
  * Created by lim on 2015-11-28.
  */
 public class ServerAdapter {
+    private Client c;
+    private int uid;
+    
+    public ServerAdapter(int uid) {
+        this.uid = uid;
+        c = new Client();
+    }
     //get recommended booth (arraylist of boothid)
     public static ArrayList<Long> getRecommendation(){
-        return new ArrayList<Long>();
+        String[] args = {"1",uid};
+        return c.send(Client.REQUEST, args);
     }
 
     //get popular booth (arraylist of boothid)
     public static ArrayList<Long> getPopular(){
-        return new ArrayList<Long>();
+        String[] args = {"0"};
+        return c.getPopular(Client.REQUEST, args);
     }
-    public static void sendLog(){
+    public static void sendLog(int booth){
+        String[] args = {"0",uid, booth};
+        c.send(Client.VISIT,args);
     }
 }
