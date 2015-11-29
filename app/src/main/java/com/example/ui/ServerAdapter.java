826aaa9384e56ce7002a74f5f781caa47d575525
boyraz <filiz.boyraz@hotmail.com>
2015-11-29 09:12:36 +0900

    Update ServerAdapter.java
    small corrections

--- app/src/main/java/com/example/ui/ServerAdapter.java
+++ app/src/main/java/com/example/ui/ServerAdapter.java
@@ -6,26 +6,29 @@ import java.util.ArrayList;
  * Created by lim on 2015-11-28.
  */
 public class ServerAdapter {
-    private Client c;
+    private GableClient c;
     private int uid;
     
     public ServerAdapter(int uid) {
         this.uid = uid;
-        c = new Client();
+        c = new GableClient();
     }
     //get recommended booth (arraylist of boothid)
-    public static ArrayList<Long> getRecommendation(){
-        String[] args = {"1",uid};
-        return c.send(Client.REQUEST, args);
+    public static ArrayList<Integer> getRecommendation(){
+        int[] args = {1,uid};
+        c.send(GableClient.REQUEST, args);
+        return c.response();
     }
 
     //get popular booth (arraylist of boothid)
-    public static ArrayList<Long> getPopular(){
-        String[] args = {"0"};
-        return c.getPopular(Client.REQUEST, args);
+    public static ArrayList<Integer> getPopular(){
+        int[] args = {0};
+        c.send(GableClient.REQUEST, args);
+        return c.response();
     }
-    public static void sendLog(int booth){
-        String[] args = {"0",uid, booth};
-        c.send(Client.VISIT,args);
+    
+    public static void sendLog(int direction, int booth){
+        int[] args = {direction,uid,booth};
+        c.send(GableClient.VISIT,args);
     }
 }
